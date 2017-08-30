68410b313102a3f4e203149ef396b0953616eae5
Andrew Zieffler <zief0002@umn.edu>
2017-08-30 16:50:40 -0500

    Added ELO rating data, updated scripts

--- scripts/elo-coaches-2017-01-24.R
+++ scripts/elo-coaches-2017-01-24.R
@@ -2,8 +2,10 @@ library(PlayerRatings)
 library(dplyr)
 library(readr)
 library(corrr)
+library(ggplot2)
 
-games = read_csv(file = "~/Documents/github/sloan-2018/data/elo-game-data.csv")
+coaches = read_csv("data/coaches_to2015.csv") %>% filter(Year >= 2005)
+games = read_csv(file = "data/elo-game-data.csv")
 head(games)
 
 
@@ -12,12 +14,31 @@ head(games)
 games_2005 = games %>% filter(year == 2005) %>% select(date, coach, coach2, coach_win)
 head(games_2005)
 
-elo_20 = elo(games_2005, init = 1300, kfac = 20, history = TRUE)
+elo_20 = elo(games_2005, init = 1500, kfac = 20, history = TRUE)
 elo20 = elo_20$ratings %>% 
   mutate(rank20 = 1:nrow(.)) %>%
   select(player = Player, rating20 = Rating, rank20)
 
-elo_25 = elo(games_2005, init = 1300, kfac = 25, history = TRUE)
+g = games %>% 
+  filter(year == 2005) %>%
+  mutate(my_gamma = ifelse(han == "Home", 65, ifelse(han == "Away", -65, 0)))
+
+nrow(games_2005)
+length(g$my_gamma)
+
+elo_20_g = elo(games_2005, init = 1500, kfac = 20, history = TRUE, gamma = g$my_gamma)
+elo20g = elo_20_g$ratings %>% 
+  mutate(rankg = 1:nrow(.)) %>%
+  select(player = Player, rating_g = Rating, rankg)
+
+glicko_20 = glicko(games_2005, init = c(1500, 100), kfac = 20, history = TRUE)
+glicko20 = glicko_20$ratings %>% 
+  mutate(rank_glicko = 1:nrow(.)) %>%
+  select(player = Player, rating_glicko = Rating, rank_glicko)
+
+
+
+elo_25 = elo(games_2005, init = 1500, kfac = 25, history = TRUE)
 elo25 = elo_25$ratings %>% 
   mutate(rank25 = 1:nrow(.)) %>%
   select(player = Player, rating25 = Rating, rank25)
@@ -27,11 +48,141 @@ elo20 %>%
   select(rank20, rank25) %>%
   correlate(method = "spearman")
 
+elo20 %>% 
+  left_join(elo20g, by = "player") %>%
+  left_join(glicko20, by = "player") %>%
+  select(rank20, rankg, rank_glicko) %>%
+  correlate(method = "spearman")
 
-elo %>% filter(Player == "Glen Mason")
+
+elo20 %>% filter(player == "Glen Mason")
+elo25 %>% filter(player == "Glen Mason")
 
 head(elo20, 20)
 tail(elo20, 20)
+
+
+# Loop to compute ELO
+
+my_elo = list(NULL)
+status = NULL
+
+for(i in 2005:2015){
+  # Get one year's data
+  games_year = games %>% 
+    filter(year == i) %>% 
+    select(date, coach, coach2, coach_win)
+
+  # Fit ELO model
+  elo_20 = elo(games_year, init = 1500, kfac = 20, history = TRUE, status = status)
+  
+  # Write output to list element
+  my_elo[[i-2004]] = elo_20$ratings %>% 
+    mutate(Rank = 1:nrow(.)) %>%
+    select(Player, Rating, Rank) %>%
+    mutate(Year = i)
+  
+  # Update initial status for following year
+  status = elo_20$ratings %>%
+    mutate(Rating = (Rating * .75 + 1500 * .25)) %>%
+    select(Player, Rating)
+  
+}
+
+
+ELO = do.call("rbind", my_elo)
+head(ELO)
+tail(ELO)
+nrow(ELO)
+
+
+ELO = ELO %>% select(Coach = Player, Year, Rating, Rank)
+head(ELO %>% arrange(Coach, Year))
+
+ELO2 = coaches %>% 
+  select(Coach, Year) %>% arrange(Coach, Year) %>%
+  left_join(ELO, by = c("Coach", "Year"))
+
+
+
+
+write.csv(ELO2, file = "data/elo-ratings.csv", row.names = FALSE)
+
+
+
+
+
+
+
+
+ELO %>% 
+  filter(Player %in% c("Glen Mason", "Kirk Ferentz", "Mack Brown", "Tim Brewster")) %>%
+  ggplot(data = ., aes(x = Year, y = Rating, color = Player)) +
+  geom_line() +
+  theme_bw()
+
+GLICKO %>% 
+  filter(Player %in% c("Glen Mason", "Kirk Ferentz", "Mack Brown", "Tim Brewster")) %>%
+  ggplot(data = ., aes(x = Year, y = Rating, color = Player)) +
+  geom_line() +
+  theme_bw()
+
+ELO2 %>% 
+  filter(Coach %in% c("Glen Mason", "Kirk Ferentz", "Mack Brown", "Tim Brewster")) %>%
+  ggplot(data = ., aes(x = Year, y = Rank, color = Coach)) +
+  geom_line() +
+  theme_bw() +
+  scale_y_reverse()
+
+GLICKO %>% 
+  filter(Player %in% c("Glen Mason", "Kirk Ferentz", "Mack Brown", "Tim Brewster")) %>%
+  ggplot(data = ., aes(x = Year, y = Rank, color = Player)) +
+  geom_line() +
+  theme_bw() +
+  scale_y_reverse()
+  
+ELO %>%
+  group_by(Year) %>%
+  summarize(M = mean(Rating), SD = sd(Rating), N = n())
+
+
+
+# Loop to compute GLICKO
+
+my_glicko = list(NULL)
+status = NULL
+
+for(i in 2005:2015){
+  # Get one year's data
+  games_year = games %>% 
+    filter(year == i) %>% 
+    select(date, coach, coach2, coach_win)
+  
+  # Fit ELO model
+  glicko_20 = glicko(games_year, init = c(1500, 200), history = TRUE, status = status)
+  
+  # Write output to list element
+  my_glicko[[i-2004]] = glicko_20$ratings %>% 
+    mutate(Rank = 1:nrow(.)) %>%
+    select(Player, Rating, Rank) %>%
+    mutate(Year = i)
+  
+  # Update initial status for following year
+  status = glicko_20$ratings %>%
+    mutate(Rating = (Rating * .75 + 1500 * .25)) %>%
+    mutate(Deviation = 200) %>%
+    select(Player, Rating, Deviation)
+  
+}
+
+
+GLICKO = do.call("rbind", my_glicko)
+head(GLICKO)
+tail(GLICKO)
+nrow(GLICKO)
+
+
+write.csv(GLICKO, file = "data/glicko-ratings.csv", row.names = FALSE)
 
 
 
--- scripts/extract-game-level-data.R
+++ scripts/extract-game-level-data.R
@@ -4,33 +4,54 @@ library(readr)
 library(dplyr)
 
 # Read in Data ----
-yby = read_csv("~/Dropbox/Football-Data/Data/year_by_year.csv", na = 'N/A')
-coaches = read_csv("~/Dropbox/Football-Data/Data/coaches.csv", na = 'N/A')
+yby = read_csv("data/year_by_year_to2015.csv") %>% filter(Year >= 2005)
+coaches = read_csv("data/coaches_to2015.csv") %>% filter(Year >= 2005)
 
-# yby <- read_csv("C:/Users/bleb/Dropbox/Football-Data/Data/year_by_year.csv", na = 'N/A')
-# coaches <- read_csv("C:/Users/bleb/Dropbox/Football-Data/Data/coaches.csv", na = 'N/A')
-# curr_coaches <- read_csv("Data/current_coaches.csv")
+
+#################################
+## Add home, neutral, away
+#################################
+
+
+## List of all locations (sites) and most common for each team (homes)
+sites = yby %>% 
+  count(Team, Location) %>%
+  group_by(Team) %>% mutate(max = max(n))
+
+homes = sites %>% filter(n == max)
+
+## Creating Home-Away-Neutral Designation
+sites = sites %>% 
+  mutate(HAN = ifelse(n == max, "Home", ifelse(Location %in% homes$Location, "Away", "Neutral")))
+
+## Adding back to game data
+yby = left_join(yby05, sites[c("Team", "Location", "HAN")], by = c("Team", "Location"))
+
+
+
+#################################
+## Create ELO-ready data
+#################################
 
 # Create num_games variable for coaches
 coaches = coaches %>%
-  group_by(Team, Year, coach) %>%
+  group_by(Team, Year, Coach) %>%
   mutate(num_games = sum(Win, Loss, Tie))
 
 # filter by year, and ensure one row per year, team, coach, combo 
 # join coaches and year by year  ----
 game_results = yby %>%
-  select(Date, Year, Team, Opponent, PF, PA) %>%
-  filter(Year >= 2005) 
+  select(Date, Year, Team, Opponent, PF, PA, HAN) 
 
 
 winners = game_results %>%
   left_join(coaches, by = c("Year", "Team")) %>%
-  select(Team, Year, Opponent, coach)
+  select(Team, Year, Opponent, Coach, han_winner = HAN)
 
 losers = game_results %>%
   select(Team2 = Team, Team = Opponent, Year) %>%
   left_join(coaches, by = c("Year", "Team")) %>%
-  select(Opponent = Team, Year, coach2 = coach, Team = Team2)
+  select(Opponent = Team, Year, coach2 = Coach, Team = Team2)
 
 coach_results = game_results %>% 
   left_join(winners, by = c("Year", "Team", "Opponent")) %>%
@@ -43,8 +64,9 @@ coach_results = game_results %>%
 
 
 coach_results$new = NA
+
 for(i in 1:nrow(coach_results)){
-  coach_results$new[i] = base::paste(sort(c(coach_results$coach[i], coach_results$coach2[i], coach_results$Date[i])), collapse = "_")
+  coach_results$new[i] = base::paste(sort(c(coach_results$Coach[i], coach_results$coach2[i], coach_results$Date[i])), collapse = "_")
 }
 
 cr2 = coach_results %>% 
@@ -55,9 +77,26 @@ cr2 = coach_results %>%
     date = as.numeric(lubridate::mdy(Date)) 
   ) %>%
   arrange(date) %>%
-  select(date, coach, coach2, coach_win, coach_margin, year = Year)
+  select(date, coach = Coach, coach2, coach_win, coach_margin, year = Year, han_winner)
 
-cr2 = cr2[complete.cases(cr2), ] %>% filter(coach != "Deleted") %>% filter(coach2 != "Deleted")
+cr2 = cr2[complete.cases(cr2), ] %>% 
+  filter(coach != "Deleted") %>% 
+  filter(coach2 != "Deleted")  %>%
+  mutate(han = ifelse(coach_win == 0 & han_winner == "Away", "Home", 
+                       ifelse(coach_win == 0 & han_winner == "Home", "Away", han_winner)
+                )
+    ) %>%
+  select(-han_winner)
+
+# coach_win == 1                    ---> han2 = han
+# coach_win == 0 & han == "Neutral" ---> han2 = "Neutral"
+# coach_win == 0 & han == "Away"    ---> han2 = "Home"
+# coach_win == 0 & han == "Home"    ---> han2 = "Away"
+
+cr2
 
 
-write.csv(cr2, file = "~/Documents/github/sloan-conference/data/elo-game-data.csv", row.names = FALSE)
+
+
+
+write.csv(cr2, file = "data/elo-game-data.csv", row.names = FALSE)
