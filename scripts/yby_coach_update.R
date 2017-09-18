07d3beee774aec327253e2d6fb97c4764f29f759
knickodem <nicko013@umn.edu>
2017-09-18 13:53:43 -0500

    yby_withcoaches updated
    The year_by_year_to2015_withcoaches file now has values for the Coach
    and Opponent_Coach columns for all games coached by a Coach who coached
    at some point between 2005 and 2015.  While there should be no NAs in
    the Coach column there are some in the Opponent_Coach column where the
    Opponent was an FCS team that we don't have data for.

--- scripts/yby_coach_update.R
+++ scripts/yby_coach_update.R
@@ -9,6 +9,8 @@ yby <- read_csv("data/year_by_year_to2015.csv", na = 'N/A')
 coaches <- read_csv("data/coaches_to2015.csv") %>%
   select(Team,Year,Coach)
 
+# earliestgame <- yby %>% group_by(Team) %>% summarize(earliest=min(Year))
+
 #### Joining files ####
 yby_coach <- yby %>% left_join(coaches,by=c("Team","Year"))
 
@@ -21,10 +23,10 @@ yby_coach_05to15 <- yby_coach %>%
   arrange(Team,Year)
 
 #### Export file to excel ####
-ybycxl <- loadWorkbook ("year_by_year_to2015_withcoaches_export.xlsx", create = TRUE)
-createSheet(ybycxl,"yby_coach_05to15")
-writeWorksheet(ybycxl,yby_coach_05to15,"yby_coach_05to15")
-saveWorkbook(ybycxl)
+# ybycxl <- loadWorkbook ("year_by_year_to2015_withcoaches_export.xlsx", create = TRUE)
+# createSheet(ybycxl,"yby_coach_05to15")
+# writeWorksheet(ybycxl,yby_coach_05to15,"yby_coach_05to15")
+# saveWorkbook(ybycxl)
 
 ## Team-Years to look for possible duplicates
 MultipleCoaches <- read_csv("data/coaches_to2015.csv") %>%
@@ -39,11 +41,38 @@ yby_coach_new <- read_excel("data/year_by_year_to2015_withcoaches_interim.xlsx")
 dupgames <- yby_coach_new %>% group_by(Team,Year,Date) %>%
   mutate(n=n()) %>% filter(n>1)
 
-## Adding Opponent coach
-opponent <- yby_coach_new %>% select(Opponent=Team,Year,Date,Opponent_Coach=Coach)
+## Adding Opponent coach from original coach data
+opponent1 <- yby_coach_new %>% select(Opponent=Team,Year,Date,Opponent_Coach1=Coach)
 yby_coach_withOPcoach <- yby_coach_new %>%
-  left_join(opponent,by=c("Opponent","Year","Date")) %>%
-  filter(!is.na(Opponent_Coach))
+  left_join(opponent1,by=c("Opponent","Year","Date"))
+
+## Checking for any remaining duplicate games
+dupgamesOPC1 <- yby_coach_withOPcoach %>% group_by(Team,Year,Date) %>%
+  mutate(n=n()) %>% filter(n>1)
+
+## Getting Opponent_Coach for games that currently have NA in the variable
+opponent2 <- coaches %>% select(Opponent=Team,Year,Opponent_Coach2=Coach)
+OPCNA <- yby_coach_withOPcoach %>% filter(is.na(Opponent_Coach1)) %>%
+  left_join(opponent2,by=c("Opponent","Year"))
+# write.csv(OPCNA,"OPCNA.csv")
+
+## Team-Years to look for possible duplicates to remove
+MultipleOppCoach<- OPCNA %>% group_by(Team,Year,Date) %>%
+  mutate(n=n()) %>% filter(n>1)
+
+## Reading in data after duplicates have been removed
+# dropping Date column because I'm having trouble converting it to character to match the other data
+OPCNA_new <- read_excel("OPCNA.xlsx",na="NA") %>% select(-Date)
+
+# ## Checking for any remaining duplicate games
+# dupgamesOPC2<- OPCNA_new %>% group_by(Team,Year,Date) %>%
+#   mutate(n=n()) %>% filter(n>1)
+
+## Adding OPC2 column and coalescing with OPC1
+yby_coach_withOPcoach <- yby_coach_withOPcoach %>% left_join(OPCNA_new) %>%
+  mutate(Opponent_Coach = coalesce(Opponent_Coach1,Opponent_Coach2)) %>%
+  select(-Opponent_Coach1,-Opponent_Coach2)
+
 
 #### Export file to excel ####
 ybywithOPxl <- loadWorkbook ("year_by_year_to2015_withcoaches.xlsx", create = TRUE)
